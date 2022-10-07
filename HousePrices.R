c7feacece0e69378d5e3da5b607fd30d63090747
beatrizeg <beatriz.eg92@gmail.com>
2022-10-07 18:09:57 +0200

    Initial commit

---
+++ HousePrices.R
@@ -0,0 +1,43 @@
+# 1. INTRODUCTION
+## 1.1. Project goal
+
+#libraries
+if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
+if(!require(stringr)) install.packages("stringr", repos = "http://cran.us.r-project.org")
+if(!require(purrr)) install.packages("purrr", repos = "http://cran.us.r-project.org")
+if(!require(ggplot2)) install.packages("ggplot2", repos = "http://cran.us.r-project.org")
+if(!require(corrplot)) install.packages("corrplot", repos = "http://cran.us.r-project.org")
+if(!require(forcats)) install.packages("forcats", repos = "http://cran.us.r-project.org")
+if(!require(rattle)) install.packages("rattle", repos = "http://cran.us.r-project.org")
+if(!require(xgboost)) install.packages("xgboost", repos = "http://cran.us.r-project.org")
+if(!require(klaR)) install.packages("klaR", repos = "http://cran.us.r-project.org")
+if(!require(tictoc)) install.packages("tictoc", repos = "http://cran.us.r-project.org")
+
+library(tidyverse)
+library(stringr)
+library(purrr)
+library(caret)
+library(ggplot2)
+library(corrplot)
+library(forcats)
+library(rattle)
+library(xgboost)
+library(klaR)
+library(tictoc)
+
+#loading databases
+train <- read_csv(file='train.csv')
+test <- read_csv(file='test.csv')
+
+train <- as.data.frame(train)
+test <- as.data.frame(test)
+
+## 1.2. Inspecting the dataset
+
+dim(train)
+summary(train)
+
+#check for NAs
+nas <- apply(train, 2, function(x) any(is.na(x)))
+nas[which(nas)]
