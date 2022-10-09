04bc3e0a30842012d1949608a5a4c0b58bf445e9
beatrizeg <beatriz.eg92@gmail.com>
2022-10-09 17:27:27 +0200

    20221009_1727

--- HousePrices.R
+++ HousePrices.R
@@ -27,8 +27,15 @@ library(klaR)
 library(tictoc)
 
 #loading databases
-main <- read_csv(file='train.csv')
-tester <- read_csv(file='test.csv')
+url_train <- "https://raw.githubusercontent.com/beatrizeg/HousePrices/main/train.csv"
+dest_file <- "./main.csv"
+download.file(url_train, destfile = dest_file)
+main <- read_csv("./main.csv")
+
+url_test <- "https://raw.githubusercontent.com/beatrizeg/HousePrices/main/test.csv"
+dest_file_tester <- "./tester.csv"
+download.file(url_test, destfile = dest_file_tester)
+test <- read_csv("./tester.csv")
 
 main <- as.data.frame(main)
 tester <- as.data.frame(test)
@@ -41,3 +48,36 @@ summary(main)
 #check for NAs
 nas <- apply(main, 2, function(x) any(is.na(x)))
 nas[which(nas)]
+
+main <- main %>% mutate(Alley=ifelse(is.na(Alley),'None',Alley),
+                        MasVnrType=ifelse(is.na(MasVnrType),'None',MasVnrType),
+                        MasVnrArea=ifelse(is.na(MasVnrArea),0,MasVnrArea),
+                        BsmtQual=ifelse(is.na(BsmtQual),'None',BsmtQual),
+                        BsmtCond=ifelse(is.na(BsmtCond),'None',BsmtCond),
+                        BsmtExposure=ifelse(is.na(BsmtExposure),'None',BsmtExposure),
+                        BsmtFinType1=ifelse(is.na(BsmtFinType1),'None',BsmtFinType1),
+                        BsmtFinType2=ifelse(is.na(BsmtFinType2),'None',BsmtFinType2),
+                        FireplaceQu=ifelse(is.na(FireplaceQu),'None',FireplaceQu),
+                        GarageType=ifelse(is.na(GarageType),'None',GarageType),
+                        GarageYrBlt=ifelse(is.na(GarageYrBlt),'None',GarageYrBlt),
+                        GarageFinish=ifelse(is.na(GarageFinish),'None',GarageFinish),
+                        GarageQual=ifelse(is.na(GarageQual),'None',GarageQual),
+                        GarageCond=ifelse(is.na(GarageCond),'None',GarageCond),
+                        PoolQC=ifelse(is.na(PoolQC),'None',PoolQC),
+                        Fence=ifelse(is.na(Fence),'None',Fence),
+                        MiscFeature=ifelse(is.na(MiscFeature),'None',MiscFeature))
+
+#check for predictors with near zero variablity
+no_var <- nearZeroVar(main, saveMetrics = TRUE)
+no_var
+
+main_m <- main %>% dplyr::select(Id, MSSubClass, MSZoning, LotFrontage, LotArea, LotShape, LandContour, Utilities, LotConfig,
+                                 LandSlope,Neighborhood,Condition1,BldgType,HouseStyle,OverallQual,OverallCond,YearBuilt,
+                                 YearRemodAdd,RoofStyle,RoofMatl,Exterior1st,Exterior2nd,MasVnrType,MasVnrArea,ExterQual,ExterCond,
+                                 Foundation,BsmtQual,
+                                 BsmtCond,BsmtExposure,BsmtFinType1,BsmtFinSF1,BsmtFinType2,BsmtFinSF2,BsmtUnfSF,TotalBsmtSF,
+                                 Heating,HeatingQC,CentralAir,Electrical,"1stFlrSF","2ndFlrSF",LowQualFinSF,GrLivArea,BsmtFullBath,
+                                 BsmtHalfBath,FullBath,HalfBath,BedroomAbvGr,KitchenAbvGr,KitchenQual,TotRmsAbvGrd,Functional,
+                                 Fireplaces,FireplaceQu,GarageType,GarageYrBlt,GarageFinish,GarageCars,GarageArea,GarageQual,
+                                 GarageCond,PavedDrive,WoodDeckSF,OpenPorchSF,EnclosedPorch,"3SsnPorch",ScreenPorch,PoolArea,
+                                 PoolQC,Fence,MiscFeature,MiscVal,MoSold,YrSold,SaleType,SaleCondition)
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # HousePrices
 This is a line from RStudio
+echo = code appears in the generated file
+eval = code is run
